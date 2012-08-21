b7619dac02b7d4bd47fd61dfdded2932914f0e24
Eric O'Connor <oconnore@gmail.com>
2012-08-21 19:29:13 -0400

    Beginning of a css encoder/decoder

---
+++ css.py
@@ -0,0 +1,26 @@
+
+import argparse, base64, sys
+
+def slurp(stream):
+    return stream.read()
+
+
+
+def run():
+    parser=argparse.ArgumentParser(description='''
+      Content Scrambling System
+    '''.strip())
+    parser.add_argument('action',choices=['enc','dec'],
+                        help='encrypt or decrypt')
+    parser.add_argument('-f','--file',dest='file',
+                        help='file to act on')
+    options=parser.parse_args()
+    if options.file:
+        stream=open(options.file,'r')
+    else:
+        stream=sys.stdin
+    cipher_bytes=slurp(stream).encode()
+    print(cipher_bytes)
+
+if __name__=='__main__':
+    run()
